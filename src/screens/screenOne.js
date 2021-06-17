25ac1a4e0e3fafb1806cb7b501d069f2da9a1917
hussein <alhussien.n.g@gmail.com>
2021-06-17 06:08:12 +0200

    s

---
+++ src/screens/screenOne.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+  const ScreenOne = (props) => {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+export default ScreenOne ;
